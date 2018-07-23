cc85de5633bbc8a66be2a94e2924f6803aea3c7c
waddell <paul.waddell@gmail.com>
2018-07-23 14:17:44 -0700

    Testing alternative specifications and choice set generation approaches

--- scripts/datasources.py
+++ scripts/datasources.py
@@ -140,7 +140,10 @@ orca.broadcast('parcels', 'buildings', cast_index=True, onto_on='parcel_id')
 orca.broadcast('buildings', 'units', cast_index=True, onto_on='building_id')
 orca.broadcast('units', 'households', cast_index=True, onto_on='unit_id')
 orca.broadcast('households', 'persons', cast_index=True, onto_on='household_id')
-
+orca.broadcast('nodeswalk', 'rentals', cast_index=True, onto_on='node_id_walk')
+orca.broadcast('nodeswalk', 'parcels', cast_index=True, onto_on='node_id_walk')
+orca.broadcast('nodessmall', 'rentals', cast_index=True, onto_on='node_id_small')
+orca.broadcast('nodessmall', 'parcels', cast_index=True, onto_on='node_id_small')
 
 #orca.broadcast('nodesdrive_vars', 'rentals', cast_on='nodesdrive_id', onto_on='nodesdrive_id')
 #orca.broadcast('nodessmall_vars', 'rentals', cast_on='nodessmall_id', onto_on='nodessmall_id')
