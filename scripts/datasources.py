6d2958dd40fb1bb2a9abc9660d8f6944078a1a82
waddell <paul.waddell@gmail.com>
2018-07-25 14:52:57 -0700

    Making notebooks more presentable

--- scripts/datasources.py
+++ scripts/datasources.py
@@ -80,7 +80,7 @@ def rentals():
 @orca.table(cache=True)
 def nodessmall():
     df = pd.read_csv(
-        d + 'nodessmall_vars.csv',
+        d + 'nodessmall_vars2.csv',
         index_col='osmid', dtype={'osmid': int})
     df.index_name = 'node_id_small'
     return df
@@ -88,7 +88,7 @@ def nodessmall():
 @orca.table(cache=True)
 def nodeswalk():
     df = pd.read_csv(
-        d + 'nodeswalk_vars.csv',
+        d + 'nodeswalk_vars2.csv',
         index_col='osmid',  dtype={'osmid': int})
     df.index_name = 'node_id_walk'
     return df
