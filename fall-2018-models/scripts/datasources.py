8f166ec740ecf28d4706dc5845db5ae87b54fc4c
Max Gardner <gardner.max@gmail.com>
2018-09-14 23:34:13 +0000

    generic modeling template notebook

--- fall-2018-models/scripts/datasources.py
+++ fall-2018-models/scripts/datasources.py
@@ -1,17 +1,14 @@
 import orca
 import numpy as np
 import pandas as pd
-import zipfile
 
-# reading data directly from zipfiles is nice, but it doesn't work with symlinks (on my 
-# Mac at least) - is there a workaround?
 
 # data documentation: https://berkeley.app.box.com/notes/282712547032
 
 
 # Set data directory
 
-d = 'data/'
+d = '/home/data/fall_2018/'
 
 if 'data_directory' in orca.list_injectables():
     d = orca.get_injectable('data_directory')
@@ -24,30 +21,21 @@ if 'data_directory' in orca.list_injectables():
 @orca.table(cache=True)
 def parcels():
     df = pd.read_csv(
-#        d + 'mtc_data_platform_format_7-6-18/' + 'parcel_attr.csv',
-        d + 'parcels_with_nodes.csv',
-        index_col='primary_id', dtype={'primary_id': int, 'block_id':str})
+        d + 'parcel_attr.csv',
+        index_col='primary_id',
+        dtype={'primary_id': int, 'block_id': str})
     return df
+
 
 @orca.table(cache=True)
 def buildings():
     df = pd.read_csv(
-        d + 'mtc_data_platform_format_7-6-18/' + 'buildings_v2.csv',
+        d + 'buildings_v2.csv',
         index_col='building_id', dtype={'building_id': int, 'parcel_id': int})
     df['res_sqft_per_unit'] = df['residential_sqft'] / df['residential_units']
     df['res_sqft_per_unit'][df['res_sqft_per_unit'] == np.inf] = 0
     return df
 
-############################################################
-
-# Tables of Residential Sales from Dataquick added by Paul Waddell
-
-#@orca.table(cache=True)
-#def sales():
-#    z = zipfile.ZipFile(d + 'salemrg.csv.zip')
-#    df = pd.read_csv(z.open('salemrg.csv'))
-#    df.index.name = 'sales_id'  # first column in CSV is unnamed
-#    return df
 
 ############################################################
 
@@ -60,41 +48,13 @@ def craigslist():
         index_col='pid', dtype={'pid': int})
     return df
 
+
 @orca.table(cache=True)
 def rentals():
     df = pd.read_csv(
-#        d + 'rental_listings_cleaned.csv',
         d + 'rentals_with_nodes.csv',
         index_col='pid', dtype={'pid': int})
     return df
-
-
-############################################################
-# Load previously tabulated accessibility vars
-#####################################################
-
-# @orca.table(cache=True)
-# def nodesdrive_vars():
-#     df = pd.read_csv(d + 'nodesdrive_vars.csv')
-#     df = df.set_index('osmid')
-#     df.index_name = 'node_id_drive'
-#     return df
-
-# @orca.table(cache=True)
-# def nodessmall():
-#     df = pd.read_csv(
-#         d + 'nodessmall_vars2.csv',
-#         index_col='osmid', dtype={'osmid': int})
-#     df.index_name = 'node_id_small'
-#     return df
-
-# @orca.table(cache=True)
-# def nodeswalk():
-#     df = pd.read_csv(
-#         d + 'nodeswalk_vars2.csv',
-#         index_col='osmid',  dtype={'osmid': int})
-#     df.index_name = 'node_id_walk'
-#     return df
 
 
 ############################################################
@@ -104,33 +64,34 @@ def rentals():
 @orca.table(cache=True)
 def units():
     df = pd.read_csv(
-        d + 'mtc_data_platform_format_7-6-18/' + 'units_v2.csv',
+        d + 'units_v2.csv',
         index_col='unit_id', dtype={'unit_id': int, 'building_id': int})
     return df
+
 
 @orca.table(cache=True)
 def households():
     df = pd.read_csv(
-        d + 'mtc_data_platform_format_7-6-18/' + 'households_v2.csv',
+        d + 'households_v2.csv',
         index_col='household_id', dtype={
-            'househould_id': int, 'block_group_id': str, 'state': str, 
+            'househould_id': int, 'block_group_id': str, 'state': str,
             'county': str, 'tract': str, 'block_group': str,
             'building_id': int, 'unit_id': int})
     return df
 
+
 @orca.table(cache=True)
 def persons():
     df = pd.read_csv(
-        d + 'mtc_data_platform_format_7-6-18/' + 'persons_v2.csv',
+        d + 'persons_v2.csv',
         index_col='person_id', dtype={'person_id': int, 'household_id': int})
     return df
 
+
 @orca.table(cache=True)
 def jobs():
-#    z = zipfile.ZipFile(d + 'jobs_w_occup.zip')
-#    df = pd.read_csv(z.open('jobs_w_occup.csv'))
     df = pd.read_csv(
-        d + 'mtc_data_platform_format_7-6-18/' + 'jobs_v2.csv',
+        d + 'jobs_v2.csv',
         index_col='job_id', dtype={'job_id': int, 'building_id': int})
     return df
 
@@ -142,11 +103,6 @@ def jobs():
 orca.broadcast('parcels', 'buildings', cast_index=True, onto_on='parcel_id')
 orca.broadcast('buildings', 'units', cast_index=True, onto_on='building_id')
 orca.broadcast('units', 'households', cast_index=True, onto_on='unit_id')
-orca.broadcast('households', 'persons', cast_index=True, onto_on='household_id')
+orca.broadcast(
+    'households', 'persons', cast_index=True, onto_on='household_id')
 orca.broadcast('buildings', 'jobs', cast_index=True, onto_on='building_id')
-
-
-#orca.broadcast('nodesdrive_vars', 'rentals', cast_on='nodesdrive_id', onto_on='nodesdrive_id')
-# orca.broadcast('nodessmall_vars', 'rentals', cast_on='nodessmall_id', onto_on='nodessmall_id')
-# orca.broadcast('nodeswalk_vars', 'rentals', cast_on='nodeswalk_id', onto_on='nodeswalk_id')
-
--- fall-2018-models/scripts/models.py
+++ fall-2018-models/scripts/models.py
@@ -8,7 +8,7 @@ reserve_num_graphs(40)
 
 # Set data directory
 
-d = 'data/'
+d = '/home/data/fall_2018/'
 
 if 'data_directory' in orca.list_injectables():
     d = orca.get_injectable('data_directory')
@@ -23,7 +23,6 @@ def test_manual_registration():
 def initialize_network_small():
     """
     This will be turned into a data loading template.
-    
     """
 
     @orca.injectable('netsmall', cache=True)
@@ -31,20 +30,26 @@ def initialize_network_small():
         nodessmall = pd.read_csv(d + 'bay_area_tertiary_strongly_nodes.csv') \
             .set_index('osmid')
         edgessmall = pd.read_csv(d + 'bay_area_tertiary_strongly_edges.csv')
-        netsmall = pdna.Network(nodessmall.x, nodessmall.y, edgessmall.u, \
-                                edgessmall.v, edgessmall[['length']], twoway=False)
+        netsmall = pdna.Network(nodessmall.x, nodessmall.y, edgessmall.u,
+                                edgessmall.v, edgessmall[['length']],
+                                twoway=False)
         netsmall.precompute(25000)
         return netsmall
 
     parcels = orca.get_table('parcels').to_frame(columns=['x', 'y'])
-    idssmall_parcel = orca.get_injectable('netsmall').get_node_ids(parcels.x, parcels.y)
+    idssmall_parcel = orca.get_injectable('netsmall').get_node_ids(
+        parcels.x, parcels.y)
     orca.add_column('parcels', 'node_id_small', idssmall_parcel, cache=False)
-    orca.broadcast('nodessmall', 'parcels', cast_index=True, onto_on='node_id_small')
+    orca.broadcast(
+        'nodessmall', 'parcels', cast_index=True, onto_on='node_id_small')
 
-    rentals = orca.get_table('rentals').to_frame(columns=['longitude', 'latitude'])
-    idssmall_rentals = orca.get_injectable('netsmall').get_node_ids(rentals.longitude, rentals.latitude)
+    rentals = orca.get_table('rentals').to_frame(
+        columns=['longitude', 'latitude'])
+    idssmall_rentals = orca.get_injectable('netsmall').get_node_ids(
+        rentals.longitude, rentals.latitude)
     orca.add_column('rentals', 'node_id_small', idssmall_rentals, cache=False)
-    orca.broadcast('nodessmall', 'rentals', cast_index=True, onto_on='node_id_small')
+    orca.broadcast(
+        'nodessmall', 'rentals', cast_index=True, onto_on='node_id_small')
 
     @orca.column('buildings', 'node_id_small')
     def node_id(parcels, buildings):
@@ -69,7 +74,8 @@ def initialize_network_small():
     # While we're at it, we can use these node_id columns to define direct broadcasts
     # between the nodes table and lower-level ones, which speeds up merging
 
-    orca.broadcast('nodessmall', 'units', cast_index=True, onto_on='node_id_small')
+    orca.broadcast(
+        'nodessmall', 'units', cast_index=True, onto_on='node_id_small')
 
 
 # @orca.step()
@@ -129,7 +135,8 @@ def initialize_network_small():
 #     def node_id(buildings, jobs):
 #         return misc.reindex(buildings.node_id_drive, jobs.building_id)
 #
-#     orca.broadcast('nodesdrive', 'units', cast_index=True, onto_on='node_id_drive')
+#     orca.broadcast(
+#        'nodesdrive', 'units', cast_index=True, onto_on='node_id_drive')
 
 
 @orca.step()
@@ -144,20 +151,25 @@ def initialize_network_walk():
         nodeswalk = pd.read_csv(d + 'bayarea_walk_nodes.csv') \
             .set_index('osmid')
         edgeswalk = pd.read_csv(d + 'bayarea_walk_edges.csv')
-        netwalk = pdna.Network(nodeswalk.x, nodeswalk.y, edgeswalk.u, \
+        netwalk = pdna.Network(nodeswalk.x, nodeswalk.y, edgeswalk.u,
                                edgeswalk.v, edgeswalk[['length']], twoway=True)
         netwalk.precompute(2500)
         return netwalk
 
     parcels = orca.get_table('parcels').to_frame(columns=['x', 'y'])
-    idswalk_parcel = orca.get_injectable('netwalk').get_node_ids(parcels.x, parcels.y)
+    idswalk_parcel = orca.get_injectable('netwalk').get_node_ids(
+        parcels.x, parcels.y)
     orca.add_column('parcels', 'node_id_walk', idswalk_parcel, cache=False)
-    orca.broadcast('nodeswalk', 'parcels', cast_index=True, onto_on='node_id_walk')
+    orca.broadcast(
+        'nodeswalk', 'parcels', cast_index=True, onto_on='node_id_walk')
 
-    rentals = orca.get_table('rentals').to_frame(columns=['longitude', 'latitude'])
-    idswalk_rentals = orca.get_injectable('netwalk').get_node_ids(rentals.longitude, rentals.latitude)
+    rentals = orca.get_table('rentals').to_frame(
+        columns=['longitude', 'latitude'])
+    idswalk_rentals = orca.get_injectable('netwalk').get_node_ids(
+        rentals.longitude, rentals.latitude)
     orca.add_column('rentals', 'node_id_walk', idswalk_rentals, cache=False)
-    orca.broadcast('nodeswalk', 'rentals', cast_index=True, onto_on='node_id_walk')
+    orca.broadcast(
+        'nodeswalk', 'rentals', cast_index=True, onto_on='node_id_walk')
 
     @orca.column('buildings', 'node_id_walk')
     def node_id(parcels, buildings):
@@ -179,32 +191,34 @@ def initialize_network_walk():
     def node_id(buildings, jobs):
         return misc.reindex(buildings.node_id_walk, jobs.building_id)
 
-    # While we're at it, we can use these node_id columns to define direct broadcasts
-    # between the nodes table and lower-level ones, which speeds up merging
+    # While we're at it, we can use these node_id columns to define direct
+    # broadcasts between the nodes table and lower-level ones, which
+    # speeds up merging
 
-    orca.broadcast('nodeswalk', 'units', cast_index=True, onto_on='node_id_walk')
+    orca.broadcast(
+        'nodeswalk', 'units', cast_index=True, onto_on='node_id_walk')
 
 
-@orca.step()
-def network_aggregations_drive(netdrive):
-    """
-    This will be turned into a network aggregation template.
-    
-    """
+# @orca.step()
+# def network_aggregations_drive(netdrive):
+#     """
+#     This will be turned into a network aggregation template.
+#     """
 
-    nodesdrive = networks.from_yaml(netdrive, 'network_aggregations_drive.yaml')
-    nodesdrive = nodesdrive.fillna(0)
-    print(nodesdrive.describe())
-    orca.add_table('nodesdrive', nodesdrive)
+#     nodesdrive = networks.from_yaml(
+#         netdrive, 'network_aggregations_drive.yaml')
+#     nodesdrive = nodesdrive.fillna(0)
+#     print(nodesdrive.describe())
+#     orca.add_table('nodesdrive', nodesdrive)
 
 
 @orca.step()
 def network_aggregations_small(netsmall):
     """
     This will be turned into a network aggregation template.
-    
     """
-    nodessmall = networks.from_yaml(netsmall, 'network_aggregations_small.yaml')
+    nodessmall = networks.from_yaml(
+        netsmall, 'network_aggregations_small.yaml')
     nodessmall = nodessmall.fillna(0)
     print(nodessmall.describe())
     orca.add_table('nodessmall', nodessmall)
