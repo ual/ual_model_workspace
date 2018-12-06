ccfc84e70e9cc38cb806665fa4eb8e6990371400
Max Gardner <gardner.max@gmail.com>
2018-12-06 20:49:23 +0000

    updated models.py w new WLCM simulate step

--- fall-2018-models/scripts/models.py
+++ fall-2018-models/scripts/models.py
@@ -1,17 +1,19 @@
 import orca
 import pandana as pdna
 import pandas as pd
-from urbansim.utils import misc, networks
-from pandana.network import reserve_num_graphs
 
-reserve_num_graphs(40)
-
+from urbansim.utils import networks
+from urbansim_templates import modelmanager as mm
+from choicemodels import MultinomialLogitResults
+from choicemodels.tools import MergedChoiceTable, simulation
 # Set data directory
 
 d = '/home/data/fall_2018/'
 
 if 'data_directory' in orca.list_injectables():
     d = orca.get_injectable('data_directory')
+
+mm.initialize()
 
 
 @orca.step()
@@ -36,66 +38,6 @@ def initialize_network_small():
         netsmall.precompute(25000)
         return netsmall
 
-# @orca.step()
-# def initialize_network_drive():
-#     """
-#     This will be turned into a data loading template.
-#
-#     """
-#
-#     @orca.injectable('netdrive', cache=True)
-#     def build_networkdrive():
-#         nodesdrive = pd.read_csv(d + 'bay_area_drive_full_nodes.csv') \
-#             .set_index('osmid')
-#         edgesdrive = pd.read_csv(d + 'bay_area_drive_full_edges.csv')
-#         netdrive = pdna.Network(nodesdrive.x, nodesdrive.y, edgesdrive.u, edgesdrive.v, \
-#                                 edgesdrive[['length']], twoway=False)
-#         netdrive.precompute(2500)
-#         return netdrive
-#
-#     # Assign 'node_id' to the parcels
-#
-#     parcels = orca.get_table('parcels').to_frame(columns=['x', 'y'])
-#     idsdrive_parcel = orca.get_injectable('netdrive').get_node_ids(parcels.x, parcels.y)
-#     orca.add_column('parcels', 'node_id_drive', idsdrive_parcel, cache=False)
-#     orca.broadcast('nodesdrive', 'parcels', cast_index=True, onto_on='node_id_drive')
-#
-#     # Assign 'node_id' to the rentals
-#
-#     rentals = orca.get_table('rentals').to_frame(columns=['longitude', 'latitude'])
-#     idsdrive_rentals = orca.get_injectable('netdrive').get_node_ids(rentals.longitude, rentals.latitude)
-#     orca.add_column('rentals', 'node_id_drive', idsdrive_rentals, cache=False)
-#     orca.broadcast('nodesdrive', 'rentals', cast_index=True, onto_on='node_id_drive')
-#
-#     # Anticipating that a 'nodes' table will be built, specify a broadcast relationship
-#     # (how to handle this best in a template?)
-#
-#     # Also assign 'node_id' down to the other tables - this is required for calculating
-#     # node-level aggregations of variables from a table
-#
-#     @orca.column('buildings', 'node_id_drive')
-#     def node_id(parcels, buildings):
-#         return misc.reindex(parcels.node_id_drive, buildings.parcel_id)
-#
-#     @orca.column('units', 'node_id_drive')
-#     def node_id(buildings, units):
-#         return misc.reindex(buildings.node_id_drive, units.building_id)
-#
-#     @orca.column('households', 'node_id_drive')
-#     def node_id(units, households):
-#         return misc.reindex(units.node_id_drive, households.unit_id)
-#
-#     @orca.column('persons', 'node_id_drive')
-#     def node_id(households, persons):
-#         return misc.reindex(households.node_id_drive, persons.household_id)
-#
-#     @orca.column('jobs', 'node_id_drive')
-#     def node_id(buildings, jobs):
-#         return misc.reindex(buildings.node_id_drive, jobs.building_id)
-#
-#     orca.broadcast(
-#        'nodesdrive', 'units', cast_index=True, onto_on='node_id_drive')
-
 
 @orca.step()
 def initialize_network_walk():
@@ -115,17 +57,23 @@ def initialize_network_walk():
         return netwalk
 
 
-# @orca.step()
-# def network_aggregations_drive(netdrive):
-#     """
-#     This will be turned into a network aggregation template.
-#     """
+@orca.step()
+def initialize_network_beam():
+    """
+    This will be turned into a data loading template.
 
-#     nodesdrive = networks.from_yaml(
-#         netdrive, 'network_aggregations_drive.yaml')
-#     nodesdrive = nodesdrive.fillna(0)
-#     print(nodesdrive.describe())
-#     orca.add_table('nodesdrive', nodesdrive)
+    """
+
+    @orca.injectable('netbeam', cache=True)
+    def build_networkbeam():
+        nodesbeam = pd.read_csv(d + 'physsim-network-nodes.csv') \
+            .set_index('id')
+        edgesbeam = pd.read_csv(d + 'physsim-network-links.csv')
+        netbeam = pdna.Network(
+            nodesbeam['x'], nodesbeam['y'], edgesbeam['from'],
+            edgesbeam['to'], edgesbeam[['travelTime']], twoway=False)
+        netbeam.precompute(7200)
+        return netbeam
 
 
 @orca.step()
@@ -151,3 +99,39 @@ def network_aggregations_walk(netwalk):
     nodeswalk = nodeswalk.fillna(0)
     print(nodeswalk.describe())
     orca.add_table('nodeswalk', nodeswalk)
+
+
+@orca.step()
+def network_aggregations_beam(netbeam):
+    """
+    This will be turned into a network aggregation template.
+
+    """
+
+    nodesbeam = networks.from_yaml(netbeam, 'network_aggregations_beam.yaml')
+    nodesbeam = nodesbeam.fillna(0)
+    print(nodesbeam.describe())
+    orca.add_table('nodesbeam', nodesbeam)
+
+
+@orca.step()
+def wlcm_simulate(persons):
+    """
+    Generate workplace location choices for the synthetic pop
+
+    """
+    interaction_terms_tt = pd.read_csv(
+        './data/WLCM_interaction_terms_tt.csv', index_col=[
+            'zone_id_home', 'zone_id_work'])
+    interaction_terms_dist = pd.read_csv(
+        './data/WLCM_interaction_terms_dist.csv', index_col=[
+            'zone_id_home', 'zone_id_work'])
+    interaction_terms_cost = pd.read_csv(
+        './data/WLCM_interaction_terms_cost.csv', index_col=[
+            'zone_id_home', 'zone_id_work'])
+
+    m = mm.get_step(
+        'WLCM_constrained-higher_ed_x_sector-tt_x_dist-cost_x_income')
+
+    m.run(chooser_batch_size=200000, interaction_terms=[
+        interaction_terms_tt, interaction_terms_dist, interaction_terms_cost])
