999c89516d8076bbf927f135c8a0571c2cc194c9
Max Gardner <gardner.max@gmail.com>
2019-05-30 06:23:04 -0700

    elcm changes

--- summer-2019-models/scripts/run_elcm_simulation_experiment.py
+++ summer-2019-models/scripts/run_elcm_simulation_experiment.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os; os.chdir('../')
 import warnings; warnings.simplefilter('ignore')
 import numpy as np
+from tqdm import tqdm
+import time
 
 from urbansim_templates import modelmanager as mm, utils
 from urbansim.models.util import columns_in_formula
@@ -29,12 +31,14 @@ csv_fnames = {
     'walk_nodes': 'bayarea_walk_nodes.csv',
     'walk_edges': 'bayarea_walk_edges.csv',
     'walk_access_vars': 'walk_net_vars.csv',
+    'access_indicators_ampeak': 'access_indicators_ampeak.csv'
 }
+outfile = './data/sampling_results_v2.csv'
 
 
-def mct_callable(obs, alts):
+def mct_callable(obs, alts, alt_sample_size):
     return MergedChoiceTable(
-        obs, alts, sample_size=m.alt_sample_size)
+        obs, alts, sample_size=alt_sample_size)
 
 
 def probs_callable(mct):
@@ -60,9 +64,14 @@ if __name__ == '__main__':
     orca.add_table('nodeswalk', walk_net_vars)
     orca.add_table('nodessmall', drive_net_vars)
 
+    try:
+        processed = pd.read_csv(outfile, index_col=0)
+    except OSError:
+        processed = pd.DataFrame()
+
     m = mm.get_step('ELCM_finance')
     m.out_chooser_filters = ['sector_id == 52']
-    m.out_alt_filters = ['non_residential_sqft > 0']
+    # m.out_alt_filters = ['non_residential_sqft > 0']
 
     observations = utils.get_data(
         tables=m.out_choosers,
@@ -80,15 +89,24 @@ if __name__ == '__main__':
 
     obs_cols = set(observations.columns) & set(expr_cols)
     observations = observations[list(obs_cols)]
+    len_choosers = len(observations)
 
     alt_cols = set(alternatives.columns) & set(expr_cols)
     alternatives = alternatives[list(alt_cols)]
     total_alts = alternatives.shape[0]
 
-    sample_sizes = [10, 50, 100, 500, 1000, 5000, 10000, 50000, 1e5]
+    sample_sizes = [
+        10, 50, 100, 500, 1000, 5000, 10000, 50000, 1e5, 5e5,
+        1e6, total_alts]
 
     outputs = {}
+
     for sample_size in sample_sizes:
+
+        sttm = time.time()
+
+        if sample_size in processed.index.values or sample_size > total_alts:
+            continue
 
         print(
             'Generating marginal probabilities using a '
@@ -97,7 +115,7 @@ if __name__ == '__main__':
         m.alt_sample_size = sample_size
         sampling_rate = m.alt_sample_size / total_alts
         full_batch_ram = sample_size * 8.8  # estimated size in MB
-        max_ram_mb = 75000
+        max_ram_mb = 50000
 
         try:
             if full_batch_ram > max_ram_mb:
@@ -107,23 +125,28 @@ if __name__ == '__main__':
                     choosers.shape[0] * (max_ram_mb / full_batch_ram))
 
                 alts = alternatives.copy()
-                len_choosers = len(choosers)
-                choices_made = 0
-                iter = 0
 
                 max_marginals = np.array([])
                 median_marginals = np.array([])
                 stddev_marginals = np.array([])
 
-                while (choices_made < len_choosers):
+                batches = choosers.groupby(
+                    np.arange(len(choosers)) // chooser_batch_size)
 
-                    iter += 1
-                    sampled_choosers = choosers.sample(
-                        int(min(chooser_batch_size, len(choosers))))
+                # while (choices_made < len_choosers):
+                for group, batch in tqdm(batches, total=len(batches)):
 
-                    mct = mct_callable(sampled_choosers, alts)
+                    # sampled_choosers = choosers.sample(
+                    #     int(min(chooser_batch_size, len(choosers))))
+                    sampled_choosers = batch
 
-                    iter_probabilities = probs_callable(mct)
+                    choicetable = mct_callable(
+                        sampled_choosers, alts, sample_size)
+
+                    assert choicetable.to_frame().shape[0] == \
+                        len(sampled_choosers) * sample_size
+
+                    iter_probabilities = probs_callable(choicetable)
                     iter_est_marginals = iter_probabilities * sampling_rate
 
                     iter_max_marginals = iter_est_marginals.groupby(
@@ -141,34 +164,30 @@ if __name__ == '__main__':
                     stddev_marginals = np.concatenate((
                         stddev_marginals, iter_stddev_marginals))
 
-                    choices_made += len(sampled_choosers)
-                    choosers = choosers.drop(sampled_choosers.index.values)
-
-                    print(
-                        "Iteration {}: {} of {} valid choices".format(
-                            iter, choices_made, len_choosers))
-
-                    if len(mct.to_frame()) == 0:
-                        print("No valid alts for the remaining choosers")
-                        break
             else:
-                choicetable = mct_callable(observations, alternatives)
+                choicetable = mct_callable(
+                    observations, alternatives, sample_size)
+                assert choicetable.to_frame().shape[0] == \
+                    len_choosers * sample_size
                 probabilities = probs_callable(choicetable)
                 est_marginal_probs = probabilities * sampling_rate
                 max_marginals = est_marginal_probs.groupby(level=0).max()
                 median_marginals = est_marginal_probs.groupby(level=0).median()
                 stddev_marginals = est_marginal_probs.groupby(level=0).std()
 
+            print('Finished after {0} minutes.'.format(
+                np.round((time.time() - sttm) / 60, 1)))
+            mean_max = np.mean(max_marginals)
+            mean_median = np.mean(median_marginals)
+            mean_stddev = np.mean(stddev_marginals)
+            outputs[sample_size] = {
+                'avg_max': mean_max, 'avg_median': mean_median,
+                'avg_stddev': mean_stddev}
+            out_df = pd.DataFrame.from_dict(outputs, orient='index')
+            out_df = pd.concat((processed, out_df))
+            out_df.to_csv(outfile)
+
         except MemoryError:
             print('MemoryError at sample size = {0}'.format(
                 str(sample_size)))
             break
-
-        mean_max = np.mean(max_marginals)
-        mean_median = np.mean(median_marginals)
-        mean_stddev = np.mean(stddev_marginals)
-        outputs[sample_size] = {
-            'avg_max': mean_max, 'avg_median': mean_median,
-            'avg_stddev': mean_stddev}
-        out_df = pd.DataFrame.from_dict(outputs, orient='index')
-        out_df.to_csv('./data/sampling_results.csv')
